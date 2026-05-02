4e93e8b476516002214c3acdb46b6709358f6c0a
magicwebkraken <terryvenusdev520@gmail.com>
2026-05-02 16:24:23 -0700

    fsdfdsfs

---
+++ _next/static/z2gUpKjM6UfV2SovyjjN8/_buildManifest.js
@@ -0,0 +1,12 @@
+(self.__BUILD_MANIFEST = {
+  __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
+  "/": [
+    "static/chunks/fea29d9f-0164ab7136d1bfe8.js",
+    "static/chunks/590-88f701b3dcf6ea12.js",
+    "static/css/028ecb519cd578ce.css",
+    "static/chunks/pages/index-34ce6bac9491aeb7.js",
+  ],
+  "/_error": ["static/chunks/pages/_error-5a00309fd5f4b49e.js"],
+  sortedPages: ["/", "/_app", "/_error"],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
